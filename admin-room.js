71b0c1922d6e9b6e3b0e44cfda70fe79d2a402bb
Linus Teklenburg <linustekl@gmail.com>
2024-05-27 21:31:05 +0200

    path fix

--- admin-room.js
+++ admin-room.js
@@ -1438,7 +1438,7 @@ onAuthStateChanged(auth, (user) => {
                                     dataG.address +
                                     ", (" +
                                     dataG.zipcode +
-                                    ")/"+originalFolderName;
+                                    ")/"+originalFolderName+"/";
                           
                                   const fileInput = document.createElement('input');
                                   fileInput.type = 'file';
