2c46d4d500251a8e5e9a2c5b9db7f59ec00c8b52
Linus Teklenburg <linustekl@gmail.com>
2024-05-29 16:00:08 +0200

    color upgrade

--- admin-room.js
+++ admin-room.js
@@ -1713,7 +1713,7 @@ onAuthStateChanged(auth, (user) => {
                               .then(() => {
 
                                 document.getElementById("info_div").innerHTML+=`<div class="div-block-34">
-                                <div class="text-block-19">${bezeichnung}</div><div class="div-block-38">
+                                <div class="text-block-19-h">${bezeichnung}</div><div class="div-block-38">
                                 <div class="text-block-19">-</div>
                                 <img src="https://assets-global.website-files.com/63ef532ba90a07a5daf4a694/664c6725c4bb3456aa3a7ce4_Edit_fill.png" loading="lazy" alt="" class="image-13 change_value">
                                 <img src="https://assets-global.website-files.com/63ef532ba90a07a5daf4a694/651da4e791f4e10b7dac637d_Trash%20(1).png" loading="lazy" alt="" class="image-13">
@@ -1754,7 +1754,7 @@ for (const info of informationArray) {
     const key = info[0];
     const value = info[1];
     var info_html=`<div class="div-block-34">
-    <div class="text-block-19">${key}</div><div class="div-block-38">
+    <div class="text-block-19-h">${key}</div><div class="div-block-38">
     <div class="text-block-19">${value}</div>
     <img src="https://assets-global.website-files.com/63ef532ba90a07a5daf4a694/664c6725c4bb3456aa3a7ce4_Edit_fill.png" loading="lazy" alt="" class="image-13 change_value">
     <img src="https://assets-global.website-files.com/63ef532ba90a07a5daf4a694/651da4e791f4e10b7dac637d_Trash%20(1).png" loading="lazy" alt="" class="image-13">
